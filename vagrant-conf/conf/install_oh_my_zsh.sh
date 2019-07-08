35dbbbdec96c116f745108c5376c43964ca96885
winkidney <winkidney@gmail.com>
2019-07-08 17:22:24 +0800

    Fix: Fix permission error for zsh-installation

--- vagrant-conf/conf/install_oh_my_zsh.sh
+++ vagrant-conf/conf/install_oh_my_zsh.sh
@@ -1,5 +1,2 @@
 #!/usr/bin/env bash
-PATH=${1}
-
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-cd ${PATH} && sudo pip install -r requirements.txt && ./install.py install_all
